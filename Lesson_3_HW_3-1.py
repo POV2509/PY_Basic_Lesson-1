4b96b6901d231c98c16cc898e97bf3b28769bffc
POV2509 <oksana.podolyan@gmail.com>
2024-10-04 00:34:32 +0300

    ДЗ 2

---
+++ Lesson_3_HW_3-1.py
@@ -0,0 +1,73 @@
+# ДЗ 3.1 Найпростіший калькулятор.
+# Виконання 4-х математичних дій (+,-,*,/) з двома операндами.
+# + зробити перевірку другого операнда - при діленні не повинен бути 0
+
+# v1
+# введення чисел - визначення зміних (перший та другий операнд)
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+
+# введення переліку математичних операцій
+print("\nCalculator")
+select_math_oper = int(input("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n"))
+print("\nSelect a mathematical operation:")
+#друк математичної операції та її результату в залежності від вибору
+if select_math_oper == 1:
+    print("Addition")
+    addition = number1 + number2
+    print("Result:",addition)
+elif select_math_oper == 2:
+    print("Subtraction")
+    subtraction = number1 - number2
+    print("Result:",subtraction)
+elif select_math_oper == 3:
+    print("Multiplication")
+    multiplication = number1 * number2
+    print("Result:",multiplication)
+elif select_math_oper == 4:
+#перевірка другого операнду - на 0 ділити не можна
+    if number2 != 0:
+        print("Division")
+        division = number1 / number2
+        print("Result:", division)
+    else:
+        print("You can't divide by 0")
+else:
+    print("Invalid input")
+
+
+# v2 з використанням конструкції match
+
+# введення чисел - визначення зміних (перший та другий операнд)
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+
+# введення переліку математичних операцій
+print("\nCalculator")
+select_math_oper = int(input("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n"))
+print("\nSelect a mathematical operation:")
+
+#друк математичної операції та її результату в залежності від вибору
+match select_math_oper:
+    case 1:
+        print("Addition")
+        addition = number1 + number2
+        print("Result:",addition)
+    case 2:
+        print("Subtraction")
+        subtraction = number1 - number2
+        print("Result:",subtraction)
+    case 3:
+        print("Multiplication")
+        multiplication = number1 * number2
+        print("Result:",multiplication)
+    case 4:
+#перевірка другого операнду - на 0 ділити не можна
+        if number2 != 0:
+            print("Division")
+            division = number1 / number2
+            print("Result:", division)
+        else:
+            print("You can't divide by 0")
+    case _:
+        print("Invalid input")
